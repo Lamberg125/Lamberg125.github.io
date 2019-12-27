96165ae2774a2685640d58d095a5bcb3d6a587be
Lamberg125 <lamberg125@yandex.ru>
2019-12-28 02:28:21 +0700

    Add files via upload

---
+++ elkipalki/php/telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot887731393:AAFYYSn_GEbe06m221hShVAXz8bxm7u0a6A/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_message'];
+$email = $_POST['user_email'];
+$token = "887731393:AAFYYSn_GEbe06m221hShVAXz8bxm7u0a6A";
+$chat_id = "-363040108";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Сообщение: ' => $user_message,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
