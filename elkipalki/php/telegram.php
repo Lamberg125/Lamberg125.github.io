d667a88ad2a9f24678aa20c326c8574e54e0eeb4
Lamberg125 <lamberg125@yandex.ru>
2019-12-29 15:12:37 +0700

    Update telegram.php

--- elkipalki/php/telegram.php
+++ elkipalki/php/telegram.php
@@ -1,29 +1,27 @@
 <?php
-
-/* https://api.telegram.org/bot887731393:AAFYYSn_GEbe06m221hShVAXz8bxm7u0a6A/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
-echo "Error";
-$name = $_POST['user_name'];
-$phone = $_POST['user_message'];
-$email = $_POST['user_email'];
+$name = $_POST['name'];
+$message = $_POST['message'];
+$email = $_POST['email'];
 $token = "887731393:AAFYYSn_GEbe06m221hShVAXz8bxm7u0a6A";
 $chat_id = "-363040108";
 $arr = array(
   'Имя пользователя: ' => $name,
-  'Сообщение: ' => $user_message,
+  'Сообщение: ' => $message,
   'Email' => $email
 );
-
+$txt = '';
 foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
-echo "Error";
-/*if ($sendToTelegram) {
-  //header('Location: thank-you.html');
-  echo "Error";
+
+if ($sendToTelegram) {
+   header('Location: ../thank-you.html');
+  //echo "ok";
+  exit;
 } else {
   echo "Error";
-}*/
+}
 ?>
+
